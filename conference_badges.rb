e52c53d865f9ffa783984f7d2155d637d9c6f284
Thomas Muscarello <muscarello.thomas@gmail.com>
2020-06-23 02:15:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,6 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-  badge_code_creator=["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  
-  badge_code_creator.each do |name|
-    return 
+def badge_code_creator(array)
+  array=[]
+  array.each do |name|
